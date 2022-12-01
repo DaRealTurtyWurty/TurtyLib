501df924d5096e5ea712e47a9b7d255185f5894a
DaRealTurtyWurty <43997792+DaRealTurtyWurty@users.noreply.github.com>
2022-12-01 10:55:42 +0000

    energy gradient, parchment, energy tooltip

--- src/main/java/io/github/darealturtywurty/turtylib/client/ui/components/EnergyWidget.java
+++ src/main/java/io/github/darealturtywurty/turtylib/client/ui/components/EnergyWidget.java
@@ -4,9 +4,12 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.datafixers.util.Either;
 import com.mojang.datafixers.util.Pair;
+import io.github.darealturtywurty.turtylib.client.util.Gradient;
+import io.github.darealturtywurty.turtylib.core.util.Either3;
 import it.unimi.dsi.fastutil.ints.Int2IntFunction;
 import net.minecraft.client.gui.components.AbstractWidget;
 import net.minecraft.client.gui.narration.NarrationElementOutput;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
@@ -15,17 +18,24 @@ import net.minecraftforge.energy.EnergyStorage;
 import java.util.function.IntSupplier;
 
 public class EnergyWidget extends AbstractWidget {
+    private final Screen screen;
+
+    private final boolean tooltip;
     private final EnergyStorage energyStorage;
     private final IntSupplier energySupplier, maxEnergySupplier;
 
     private final ResourceLocation texture;
     private final Int2IntFunction color;
+    private final Gradient gradient;
 
-
-    private EnergyWidget(int x, int y, int width, int height, Either<ResourceLocation, Int2IntFunction> textureOrColor, Either<EnergyStorage, Pair<IntSupplier, IntSupplier>> energyStorage) {
+    private EnergyWidget(Screen screen, int x, int y, int width, int height, boolean tooltip, Either3<ResourceLocation, Int2IntFunction, Gradient> textureOrColorOrGradient, Either<EnergyStorage, Pair<IntSupplier, IntSupplier>> energyStorage) {
         super(x, y, width, height, Component.empty());
-        this.texture = textureOrColor.map(texture -> texture, color -> null);
-        this.color = textureOrColor.map(texture -> null, color -> color);
+        this.screen = screen;
+
+        this.tooltip = tooltip;
+        this.texture = textureOrColorOrGradient.map(texture -> texture, color -> null, gradient -> null);
+        this.color = textureOrColorOrGradient.map(texture -> null, color -> color, gradient -> null);
+        this.gradient = textureOrColorOrGradient.map(texture -> null, color -> null, gradient -> gradient);
         this.energyStorage = energyStorage.map(storage -> storage, pair -> null);
         this.energySupplier = energyStorage.map(storage -> null, Pair::getFirst);
         this.maxEnergySupplier = energyStorage.map(storage -> null, Pair::getSecond);
@@ -48,18 +58,37 @@ public class EnergyWidget extends AbstractWidget {
             RenderSystem.setShaderTexture(0, this.texture);
 
             blit(pPoseStack, this.x, this.y + this.height - energyHeight, 0, 0, this.width, energyHeight);
-            return;
+        } else if (gradient != null) {
+            this.gradient.draw(pPoseStack, this.x, this.y + this.height - energyHeight, this.x + this.width,
+                    this.y + this.height);
+        } else {
+            fill(pPoseStack, this.x, this.y + this.height - energyHeight, this.x + this.width, this.y + this.height,
+                    this.color.applyAsInt(energy));
         }
 
-        fill(pPoseStack, this.x, this.y + this.height - energyHeight, this.x + this.width, this.y + this.height,
-                this.color.applyAsInt(energy));
+        if (this.tooltip) {
+            renderToolTip(pPoseStack, pMouseX, pMouseY);
+        }
+    }
+
+    @Override
+    public void renderToolTip(PoseStack pPoseStack, int pMouseX, int pMouseY) {
+        if (!isMouseOver(pMouseX, pMouseY)) return;
+
+        int energy = this.energyStorage != null ? this.energyStorage.getEnergyStored() : this.energySupplier.getAsInt();
+        int maxEnergy = this.energyStorage != null ? this.energyStorage.getMaxEnergyStored() : this.maxEnergySupplier.getAsInt();
+        this.screen.renderTooltip(pPoseStack, Component.nullToEmpty(energy + " / " + maxEnergy + " FE"), pMouseX,
+                pMouseY);
     }
 
     public static class Builder {
+        private final int x, y, width, height;
+
+        private boolean tooltip = false;
         private Either<EnergyStorage, Pair<IntSupplier, IntSupplier>> energyStorage = Either.right(
                 Pair.of(() -> 0, () -> 0));
-        private Either<ResourceLocation, Int2IntFunction> textureOrColor = Either.right(i -> 0xFFFF0000);
-        private final int x, y, width, height;
+        private Either3<ResourceLocation, Int2IntFunction, Gradient> textureOrColorOrGradient = Either3.middle(
+                i -> 0xFFFF0000);
 
         public Builder(int x, int y, int width, int height) {
             this.x = x;
@@ -79,17 +108,28 @@ public class EnergyWidget extends AbstractWidget {
         }
 
         public Builder texture(ResourceLocation texture) {
-            this.textureOrColor = Either.left(texture);
+            this.textureOrColorOrGradient = Either3.left(texture);
             return this;
         }
 
         public Builder color(Int2IntFunction color) {
-            this.textureOrColor = Either.right(color);
+            this.textureOrColorOrGradient = Either3.middle(color);
             return this;
         }
 
-        public EnergyWidget build() {
-            return new EnergyWidget(this.x, this.y, this.width, this.height, this.textureOrColor, this.energyStorage);
+        public Builder gradient(Gradient gradient) {
+            this.textureOrColorOrGradient = Either3.right(gradient);
+            return this;
+        }
+
+        public Builder tooltip() {
+            this.tooltip = true;
+            return this;
+        }
+
+        public EnergyWidget build(Screen screen) {
+            return new EnergyWidget(screen, this.x, this.y, this.width, this.height, this.tooltip,
+                    this.textureOrColorOrGradient, this.energyStorage);
         }
     }
 }
---
+++ src/main/java/io/github/darealturtywurty/turtylib/client/util/Gradient.java
@@ -0,0 +1,127 @@
+package io.github.darealturtywurty.turtylib.client.util;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.*;
+import com.mojang.math.Matrix4f;
+import net.minecraft.client.renderer.GameRenderer;
+
+public abstract class Gradient {
+    protected abstract int getTopLeftColor();
+
+    protected abstract int getTopRightColor();
+
+    protected abstract int getBottomLeftColor();
+
+    protected abstract int getBottomRightColor();
+
+    private static final class Gradient2 extends Gradient {
+        private final int startColor, endColor;
+
+        private Gradient2(int startColor, int endColor) {
+            this.startColor = startColor;
+            this.endColor = endColor;
+        }
+
+        @Override
+        protected int getTopLeftColor() {
+            return this.startColor;
+        }
+
+        @Override
+        protected int getTopRightColor() {
+            return this.startColor;
+        }
+
+        @Override
+        protected int getBottomLeftColor() {
+            return this.endColor;
+        }
+
+        @Override
+        protected int getBottomRightColor() {
+            return this.endColor;
+        }
+    }
+
+    private static final class Gradient4 extends Gradient {
+        private final int topLeftColor, topRightColor, bottomLeftColor, bottomRightColor;
+
+        private Gradient4(int topLeftColor, int topRightColor, int bottomLeftColor, int bottomRightColor) {
+            this.topLeftColor = topLeftColor;
+            this.topRightColor = topRightColor;
+            this.bottomLeftColor = bottomLeftColor;
+            this.bottomRightColor = bottomRightColor;
+        }
+
+        @Override
+        protected int getTopLeftColor() {
+            return this.topLeftColor;
+        }
+
+        @Override
+        protected int getTopRightColor() {
+            return this.topRightColor;
+        }
+
+        @Override
+        protected int getBottomLeftColor() {
+            return this.bottomLeftColor;
+        }
+
+        @Override
+        protected int getBottomRightColor() {
+            return this.bottomRightColor;
+        }
+    }
+
+    public static Gradient of(int startColor, int endColor) {
+        return new Gradient2(startColor, endColor);
+    }
+
+    public static Gradient of(int topLeftColor, int topRightColor, int bottomLeftColor, int bottomRightColor) {
+        return new Gradient4(topLeftColor, topRightColor, bottomLeftColor, bottomRightColor);
+    }
+
+    public void draw(PoseStack poseStack, int x0, int y0, int x1, int x2) {
+        draw(poseStack.last().pose(), x0, y0, x1, x2, getTopLeftColor(), getTopRightColor(), getBottomLeftColor(),
+                getBottomRightColor());
+    }
+
+    private static void draw(Matrix4f matrix4f, int x0, int y0, int x1, int y1, int topLeftColor, int topRightColor, int bottomLeftColor, int bottomRightColor) {
+        // Setup
+        RenderSystem.disableTexture();
+        RenderSystem.enableBlend();
+        RenderSystem.defaultBlendFunc();
+        RenderSystem.setShader(GameRenderer::getPositionColorShader);
+        Tesselator tesselator = Tesselator.getInstance();
+        BufferBuilder bufferBuilder = tesselator.getBuilder();
+        bufferBuilder.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_COLOR);
+
+        // Draw
+        float f = (float) (topLeftColor >> 24 & 255) / 255.0F;
+        float f1 = (float) (topLeftColor >> 16 & 255) / 255.0F;
+        float f2 = (float) (topLeftColor >> 8 & 255) / 255.0F;
+        float f3 = (float) (topLeftColor & 255) / 255.0F;
+        float f4 = (float) (topRightColor >> 24 & 255) / 255.0F;
+        float f5 = (float) (topRightColor >> 16 & 255) / 255.0F;
+        float f6 = (float) (topRightColor >> 8 & 255) / 255.0F;
+        float f7 = (float) (topRightColor & 255) / 255.0F;
+        float f8 = (float) (bottomLeftColor >> 24 & 255) / 255.0F;
+        float f9 = (float) (bottomLeftColor >> 16 & 255) / 255.0F;
+        float f10 = (float) (bottomLeftColor >> 8 & 255) / 255.0F;
+        float f11 = (float) (bottomLeftColor & 255) / 255.0F;
+        float f12 = (float) (bottomRightColor >> 24 & 255) / 255.0F;
+        float f13 = (float) (bottomRightColor >> 16 & 255) / 255.0F;
+        float f14 = (float) (bottomRightColor >> 8 & 255) / 255.0F;
+        float f15 = (float) (bottomRightColor & 255) / 255.0F;
+        bufferBuilder.vertex(matrix4f, (float) x1, (float) y0, 0).color(f5, f6, f7, f4).endVertex();
+        bufferBuilder.vertex(matrix4f, (float) x0, (float) y0, 0).color(f1, f2, f3, f).endVertex();
+        bufferBuilder.vertex(matrix4f, (float) x0, (float) y1, 0).color(f9, f10, f11, f8).endVertex();
+        bufferBuilder.vertex(matrix4f, (float) x1, (float) y1, 0).color(f13, f14, f15, f12).endVertex();
+
+        // Cleanup
+        tesselator.end();
+        RenderSystem.disableBlend();
+        RenderSystem.enableTexture();
+    }
+}
