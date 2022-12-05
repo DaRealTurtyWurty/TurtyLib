c9698f9570e58f514eefe3b08f1c3fe7e114d3cb
DaRealTurtyWurty <43997792+DaRealTurtyWurty@users.noreply.github.com>
2022-12-05 00:15:13 +0000

    Modifications SimpleJsonDataManager

--- src/main/java/io/github/darealturtywurty/turtylib/core/data/SimpleJsonDataManager.java
+++ src/main/java/io/github/darealturtywurty/turtylib/core/data/SimpleJsonDataManager.java
@@ -1,17 +1,17 @@
 package io.github.darealturtywurty.turtylib.core.data;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.function.BiFunction;
-
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
-
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.server.packs.resources.SimpleJsonResourceReloadListener;
 import net.minecraft.util.profiling.ProfilerFiller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.BiFunction;
 
 public class SimpleJsonDataManager<T> extends SimpleJsonResourceReloadListener {
     private static final Gson GSON = new GsonBuilder().disableHtmlEscaping().create();
@@ -24,8 +24,7 @@ public class SimpleJsonDataManager<T> extends SimpleJsonResourceReloadListener {
         this.dataClass = dataClass;
     }
 
-    public Map<ResourceLocation, T> mapValues(Map<ResourceLocation, JsonElement> inputs,
-            BiFunction<ResourceLocation, JsonElement, T> mapper) {
+    public Map<ResourceLocation, T> mapValues(Map<ResourceLocation, JsonElement> inputs, BiFunction<ResourceLocation, JsonElement, T> mapper) {
         final Map<ResourceLocation, T> newMap = new HashMap<>();
 
         inputs.forEach((key, input) -> newMap.put(key, mapper.apply(key, input)));
@@ -40,5 +39,17 @@ public class SimpleJsonDataManager<T> extends SimpleJsonResourceReloadListener {
 
     protected T getJsonAsData(ResourceLocation loc, JsonElement json) {
         return GSON.fromJson(json, this.dataClass);
+    }
+
+    public Map<ResourceLocation, T> getDataMap() {
+        return this.data;
+    }
+
+    public T getData(ResourceLocation loc) {
+        return this.data.get(loc);
+    }
+
+    public List<T> getDataList() {
+        return List.copyOf(this.data.values());
     }
 }
